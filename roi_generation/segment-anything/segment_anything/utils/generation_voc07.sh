82c09fbce08c44a2e66f6b27e5051075ad5c13ca
Junwei Z <96565109+Colezwhy@users.noreply.github.com>
2024-02-22 23:33:32 +0800

    update path and scripts

--- roi_generation/segment-anything/segment_anything/utils/generation_voc07.sh
+++ roi_generation/segment-anything/segment_anything/utils/generation_voc07.sh
@@ -1,9 +1,9 @@
 #------------------------------- trainval set ----------------------------------#
-python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/WeakTr/voc07/trainval.txt \
+python ./segment-anything/segment_anything/utils/pooled_proposal_generation.py --img-set-path ./WeakTr/voc07/trainval.txt \
 --peakfile-name coarse/peak-pam-k17-t90/ \
---txt-folder-path /home/junweizhou/WeakTr/WeakTr/weaktr_results/VOC07-peak \
---ori-image-path /home/junweizhou/WeakTr/WeakTr/data/voc07/VOCdevkit/VOC2007/JPEGImages \
---proposal-storage-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/coarse \
+--txt-folder-path ./WeakTr/weaktr_results/VOC07-peak \
+--ori-image-path ../data/voc07/VOC2007/JPEGImages \
+--proposal-storage-path ./segment-anything/peak_proposal/VOC07/coarse \
 --proposal-name k17_t90.pkl \
 --device 0 \
 --starting-layer 1 \
@@ -14,11 +14,11 @@ python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/Weak
 --is-cls-aware True \
 
 
-python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/WeakTr/voc07/trainval.txt \
+python ./segment-anything/segment_anything/utils/pooled_proposal_generation.py --img-set-path ./WeakTr/voc07/trainval.txt \
 --peakfile-name fine/peak-pam-k17-t90/ \
---txt-folder-path /home/junweizhou/WeakTr/WeakTr/weaktr_results/VOC07-peak \
---ori-image-path /home/junweizhou/WeakTr/WeakTr/data/voc07/VOCdevkit/VOC2007/JPEGImages \
---proposal-storage-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/fine \
+--txt-folder-path ./WeakTr/weaktr_results/VOC07-peak \
+--ori-image-path ../data/voc07/VOC2007/JPEGImages \
+--proposal-storage-path ./segment-anything/peak_proposal/VOC07/fine \
 --proposal-name k17_t90.pkl \
 --device 0 \
 --starting-layer 1 \
@@ -29,11 +29,11 @@ python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/Weak
 --is-cls-aware True \
 
 
-python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/WeakTr/voc07/trainval.txt \
+python ./segment-anything/segment_anything/utils/pooled_proposal_generation.py --img-set-path ./WeakTr/voc07/trainval.txt \
 --peakfile-name cross/peak-pam-k129-t90/ \
---txt-folder-path /home/junweizhou/WeakTr/WeakTr/weaktr_results/VOC07-peak \
---ori-image-path /home/junweizhou/WeakTr/WeakTr/data/voc07/VOCdevkit/VOC2007/JPEGImages \
---proposal-storage-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/cross \
+--txt-folder-path ./WeakTr/weaktr_results/VOC07-peak \
+--ori-image-path ../data/voc07/VOC2007/JPEGImages \
+--proposal-storage-path ./segment-anything/peak_proposal/VOC07/cross \
 --proposal-name k129_t90_wopost.pkl \
 --device 0 \
 --starting-layer 4 \
@@ -43,22 +43,22 @@ python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/Weak
 --n-processes 12 \
 
 
-python post_suppression.py --target-prop /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/cross/k129_t90_wopost.pkl \
+python ./segment-anything/segment_anything/utils/post_suppression.py --target-prop ./segment-anything/peak_proposal/VOC07/cross/k129_t90_wopost.pkl \
 --threshold 0.9 \
 --save-name k129_t90.pkl \
---save-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/cross \
+--save-path ./segment-anything/peak_proposal/VOC07/cross \
 
-python proposal_concatenation.py --pickle-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/ \
---storage-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/ \
+python ./segment-anything/segment_anything/utils/proposal_concatenation.py --pickle-path ./segment-anything/peak_proposal/VOC07/ \
+--storage-path ./segment-anything/peak_proposal/VOC07/ \
 --file-name voc07trainval.pkl \
 
 #---------------------------------------test set------------------------------------------#
 
-python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/WeakTr/voc07/test.txt \
+python ./segment-anything/segment_anything/utils/pooled_proposal_generation.py --img-set-path ./WeakTr/voc07/test.txt \
 --peakfile-name fine/peak-pam-k17-t90_test/ \
---txt-folder-path /home/junweizhou/WeakTr/WeakTr/weaktr_results/VOC07-peak \
---ori-image-path /home/junweizhou/WeakTr/WeakTr/data/voc07/VOCdevkit/VOC2007/JPEGImages \
---proposal-storage-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/fine \
+--txt-folder-path ./WeakTr/weaktr_results/VOC07-peak \
+--ori-image-path ../data/voc07/VOC2007/JPEGImages \
+--proposal-storage-path ./segment-anything/peak_proposal/VOC07/fine \
 --proposal-name k17_t90_test.pkl \
 --device 0 \
 --starting-layer 1 \
@@ -68,11 +68,11 @@ python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/Weak
 --n-processes 12 \
 --is-cls-aware True \
 
-python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/WeakTr/voc07/test.txt \
+python ./segment-anything/segment_anything/utils/pooled_proposal_generation.py --img-set-path ./WeakTr/voc07/test.txt \
 --peakfile-name coarse/peak-pam-k17-t90_test/ \
---txt-folder-path /home/junweizhou/WeakTr/WeakTr/weaktr_results/VOC07-peak \
---ori-image-path /home/junweizhou/WeakTr/WeakTr/data/voc07/VOCdevkit/VOC2007/JPEGImages \
---proposal-storage-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/coarse \
+--txt-folder-path ./WeakTr/weaktr_results/VOC07-peak \
+--ori-image-path ../data/voc07/VOC2007/JPEGImages \
+--proposal-storage-path ./segment-anything/peak_proposal/VOC07/coarse \
 --proposal-name k17_t90_test.pkl \
 --device 0 \
 --starting-layer 1 \
@@ -82,11 +82,11 @@ python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/Weak
 --n-processes 12 \
 --is-cls-aware True \
 
-python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/WeakTr/voc07/test.txt \
+python ./segment-anything/segment_anything/utils/pooled_proposal_generation.py --img-set-path ./WeakTr/voc07/test.txt \
 --peakfile-name cross/peak-pam-k129-t90_test/ \
---txt-folder-path /home/junweizhou/WeakTr/WeakTr/weaktr_results/VOC07-peak \
---ori-image-path /home/junweizhou/WeakTr/WeakTr/data/voc07/VOCdevkit/VOC2007/JPEGImages \
---proposal-storage-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/cross \
+--txt-folder-path ./WeakTr/weaktr_results/VOC07-peak \
+--ori-image-path ../data/voc07/VOC2007/JPEGImages \
+--proposal-storage-path ./segment-anything/peak_proposal/VOC07/cross \
 --proposal-name k129_t90_wopost_test.pkl \
 --device 0 \
 --starting-layer 4 \
@@ -95,13 +95,13 @@ python pooled_proposal_generation.py --img-set-path /home/junweizhou/WeakTr/Weak
 --num-gpus 4 \
 --n-processes 12 \
 
-python post_suppression.py --target-prop /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/cross/k129_t90_wopost.pkl \
+python ./segment-anything/segment_anything/utils/post_suppression.py --target-prop ./segment-anything/peak_proposal/VOC07/cross/k129_t90_wopost.pkl \
 --threshold 0.9 \
 --save-name k129_t90_test.pkl \
---save-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/cross \
+--save-path ./segment-anything/peak_proposal/VOC07/cross \
 
-python proposal_concatenation.py --pickle-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/ \
---storage-path /home/junweizhou/WeakSAM/segment-anything/peak_proposal/VOC07/ \
+python ./segment-anything/segment_anything/utils/proposal_concatenation.py --pickle-path ./segment-anything/peak_proposal/VOC07/ \
+--storage-path ./segment-anything/peak_proposal/VOC07/ \
 --file-name voc07test.pkl \
 --istest True
 
